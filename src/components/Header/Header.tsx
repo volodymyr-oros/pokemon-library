ac38fbc144ec95307728606cb2a506fc721a51ea
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-07-13 17:13:12 +0300

    feat: Add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import logo from "./assets/logo.png";
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <div className={`${styles.header__logo} ${styles.logo}`}>
+        <a href="/" className={styles.logo__link} aria-label="Go to homepage">
+          <img
+            src={logo}
+            className={styles.logo__img}
+            alt="Pokemon Library Logo"
+            width="150"
+            height="auto"
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
