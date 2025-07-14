d2da2bf652b2edf17782ff5861224dd23593242e
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-07-14 12:08:10 +0300

    refactor: update global styles entry point and font setup
    - Replaced import of reset.scss with main.scss in main.tsx
    - Added font declarations in fonts.scss
    - Updated _variables.scss with unified imports
    - Created main.scss as central entry for global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-const container = document.getElementById('root');
+// import "./styles/reset.scss";
+import "./styles/main.scss";
+
+const container = document.getElementById("root");
 const root = createRoot(container!);
 
 root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
